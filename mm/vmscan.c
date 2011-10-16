41200c11e1b0d2a62ab3bd3a2b38ca32a5589712
LorD ClockaN <davor@losinj.com>
2011-10-16 15:29:47 +0200

    add swappiness value of 50 only if ZRAM is enabled

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -124,7 +124,13 @@ struct scan_control {
 /*
  * From 0 .. 100.  Higher means more swappy.
  */
-int vm_swappiness = 60;
+
+#ifdef CONFIG_ZRAM
+int vm_swappiness = 50;
+#else
+int vm_swappiness;
+#endif
+
 long vm_total_pages;	/* The total number of pages which the VM controls */
 
 static LIST_HEAD(shrinker_list);
